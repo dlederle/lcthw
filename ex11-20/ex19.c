e18cb1f8be1fd58fef3f3740bfddb86cd6545d96
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2017-08-15 12:20:41 -0400

    Old code for Learn C the Hard Way

---
+++ ex11-20/ex19.c
@@ -0,0 +1,13 @@
+#include "dbg.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void test_debug()
+{
+        debug("I have brown hair");
+
+        debug("I am %d years old", 25);
+        debug("Test", %s);
+}
+
+
